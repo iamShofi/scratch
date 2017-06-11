7f4ef22b4f156d9c49260d905f11d74009f5a6e5
iamShofi <shofi999@gmail.com>
2017-06-12 01:20:27 +0600

    Scratch v1.0

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+	def index
+		@content_first = 'this is something';
+		@content_second = 'this is something else';	
+	end
+	
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def update
+	end
+
+	def show
+
+	end
+
+	def destroy
+
+	end	
+
+end
